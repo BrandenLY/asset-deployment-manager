b18231dd7a0b986b332f8e894e1513aadad8f08a
Branden Yarbrough <brandeny@bubblebinz.com>
2023-12-15 11:12:06 -0800

    Register Location Model in django admin site.

--- assets/admin.py
+++ assets/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from .models import Asset
 from .models import AssetModel
+from .models import Location
 from .models import Shipment
 # Register your models here.
 
 admin.site.register(Asset)
 admin.site.register(AssetModel)
+admin.site.register(Location)
 admin.site.register(Shipment)
