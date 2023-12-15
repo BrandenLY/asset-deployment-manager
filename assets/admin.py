88341b0eb87f38c9505f322e862ef3b416ef6694
Branden Yarbrough <brandeny@bubblebinz.com>
2023-12-15 10:53:56 -0800

    Register Asset Model in django admin site.

--- assets/admin.py
+++ assets/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from .models import Asset
+from .models import AssetModel
 from .models import Shipment
 # Register your models here.
 
 admin.site.register(Asset)
+admin.site.register(AssetModel)
 admin.site.register(Shipment)
