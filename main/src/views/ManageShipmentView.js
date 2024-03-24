75d8ef66281943167f619509a06ae91a315564b2
Branden Yarbrough <brandenly@gmail.com>
2024-03-24 02:56:44 -0700

    Add 'create new shipment' dialog.

--- main/src/views/ManageShipmentView.js
+++ main/src/views/ManageShipmentView.js
@@ -1,19 +1,21 @@
 import React, {useEffect, useState, useRef, useContext} from "react";
-import { Box, Button, Link, Fab} from "@mui/material";
-import { Add } from '@mui/icons-material';
+import { Box, Button, Dialog, Link, Fab, Paper, Typography, Grid, IconButton, Divider, Container, List, ListItem} from "@mui/material";
+import { Add, Close } from '@mui/icons-material';
 import SortingGrid from "../components/SortingGrid";
-import { useBackend } from "../customHooks";
+import { useBackend, useModelFormFields } from "../customHooks";
 import {
     Link as RouterLink,
   } from 'react-router-dom';
-import { useQuery } from "@tanstack/react-query";
-import { backendApiContext } from "../context";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { backendApiContext, getCookie } from "../context";
+
 
 const ManageShipmentView = props => {
 
-    // Context
+    // State
+    const [displayCreateShipmentDialog, setDisplayCreateShipmentDialog] = useState(false);
     const backendCtx = useContext(backendApiContext);
-
+    
     // Retrieve Shipment Data
     const {
         data:shipmentData,
@@ -57,7 +59,7 @@ const ManageShipmentView = props => {
                 sx={{
                     display: "flex",
                     justifyContent: "flex-end",
-                    marginBottom: 1
+                    transform: "translateY(-18px)",
                 }}
             >
                 <Fab 
@@ -65,6 +67,7 @@ const ManageShipmentView = props => {
                     variant="extended"
                     size="small"
                     color="primary"
+                    onClick={() => setDisplayCreateShipmentDialog(true)}
                 >
                     New shipment
                     <Add />
@@ -76,7 +79,123 @@ const ManageShipmentView = props => {
                 initialColumns={initialColumns}
                 data={shipmentData?.pages.map(p => p.results).flat()}
             />
+            <CreateShipmentDialog open={displayCreateShipmentDialog} onClose={() => setDisplayCreateShipmentDialog(!displayCreateShipmentDialog)} addNotif={props.addNotif}/>
         </Box>
+    )
+}
+
+const mutationCreateFn = async ({model, data}) =>{
+
+    const updateUrl = new URL(`${window.location.protocol}${window.location.host}/api/${model.modelName}/${data.id ? data.id + '/' : ''}`)
+    const requestHeaders = new Headers();
+    requestHeaders.set('Content-Type', 'application/json');
+    requestHeaders.set('X-CSRFToken', getCookie('csrftoken'))
+
+    console.log(JSON.stringify(data))
+    return fetch( updateUrl, {method:"POST", headers:requestHeaders, body:JSON.stringify(data)} )
+}
+
+const CreateShipmentDialog = props =>{
+
+    const {onClose, open} = props;
+    const {models} = useContext(backendApiContext);
+    const {fields, addFieldErrors, clearErrors} = useModelFormFields({model:models.shipment, excludeReadOnly:true});
+
+    const {mutate} = useMutation({
+        mutationFn: mutationCreateFn,
+        onSettled: (data, error, variables) => {
+            if (data.ok){
+                props.addNotif({message:'Successfully created shipment'});
+            } else {
+                props.addNotif({message:'Failed to create shipment', severity:'error'})
+                data.json().then(data => {
+                    Object.entries(data).forEach( ([fieldName,fieldErrors]) => {
+                        fieldErrors.forEach(error => {
+                            addFieldErrors(fieldName, error)
+                        });
+                    })
+                })
+            }
+        }
+    });
+
+    const createNewShipment = () => {
+        clearErrors()
+        let payload = {};
+        models.shipment.fields.forEach(f => {
+
+            if (f.related){
+                payload[f.name] = fields[f.name]?.currentValue?.[f.related.returnPropertyName]
+            }
+            else if(f.inputType == 'date' && fields[f.name]?.currentValue){
+                payload[f.name] = new Date(fields[f.name]?.currentValue)
+            }
+            else if(f.options){
+                payload[f.name] = fields[f.name]?.currentValue?.['id']
+            }
+            else{
+                payload[f.name] = fields[f.name]?.currentValue
+            }
+
+        });
+        
+        mutate({model: models.shipment, data:payload, addFieldErrors})
+    }
+
+    return(
+        <Dialog onClose={onClose} open={open} maxWidth="sm" fullWidth> {console.log(fields)}
+            <Box sx={{padding:2, width:'100%'}}>
+                <Grid container spacing={2}>
+                    <Grid item xs={12}>
+                        <Box sx={{display: 'flex', alignItems: 'center', gap:2, justifyContent:'space-between'}}>
+                            <Box>
+                                <Typography variant='h6'>Create a shipment</Typography>
+                                <Typography variant='subtitle2' sx={{fontWeight:400}}>Setup an outbound shipment.</Typography>
+                            </Box>
+                            <IconButton onClick={onClose}>
+                                <Close />
+                            </IconButton>
+                        </Box>
+                    </Grid>
+                    <Grid item xs={6}>
+                        {fields?.['status']?.['component']}
+                        {fields?.['status']?.['errors'].map(e => <Typography sx={{color: "error.main", margin:1}}>{e}</Typography>)}
+                    </Grid> <Grid item xs={6}/>
+                    <Grid item xs={6}>
+                        {fields?.['carrier']?.['component']}
+                        {fields?.['carrier']?.['errors'].map(e => <Typography sx={{color: "error.main", margin:1}}>{e}</Typography>)}
+                    </Grid> <Grid item xs={6}/>
+                    <Grid item xs={6}>
+                        {fields?.['origin']?.['component']}
+                        {fields?.['origin']?.['errors'].map(e => <Typography sx={{color: "error.main", margin:1}}>{e}</Typography>)}
+                    </Grid>
+                    <Grid item xs={6}>
+                        {fields?.['destination']?.['component']}
+                        {fields?.['destination']?.['errors'].map(e => <Typography sx={{color: "error.main", margin:1}}>{e}</Typography>)}
+                    </Grid>
+                    <Grid item xs={6}>
+                        {fields?.['arrival_date']?.['component']}
+                        {fields?.['arrival_date']?.['errors'].map(e => <Typography sx={{color: "error.main", margin:1}}>{e}</Typography>)}
+                    </Grid>
+                    <Grid item xs={6}>
+                        {fields?.['departure_date']?.['component']}
+                        {fields?.['departure_date']?.['errors'].map(e => <Typography sx={{color: "error.main", margin:1}}>{e}</Typography>)}
+                    </Grid>
+                    <Grid item xs={6}>
+                        {fields?.['send_back_shipment']?.['component']}
+                        {fields?.['send_back_shipment']?.['errors'].map(e => <Typography sx={{color: "error.main", margin:1}}>{e}</Typography>)}
+                    </Grid>
+                    <Grid item xs={12}>
+
+                    </Grid>
+                </Grid>
+            </Box>
+            <Container sx={{width: "100%", display: "flex", justifyContent:"center", paddingBottom:2}}>
+                <Button variant="contained" onClick={createNewShipment}>
+                    Submit
+                </Button>
+            </Container>
+        </Dialog>
     )
 }
 
