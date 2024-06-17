28fb369cb9dc244944eab046ae86483e43f6184e
Branden Yarbrough <brandenly@gmail.com>
2024-06-16 22:23:19 -0700

    update imports

--- main/src/index.js
+++ main/src/index.js
@@ -1,16 +1,11 @@
 import React from "react";
-import { createRoot } from 'react-dom/client';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from "react-router-dom";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 // Material UI
-import createTheme from "@mui/material";
-import CssBaseline from '@mui/material/CssBaseline';
-import ThemeProvider from "@mui/material";
+import {createTheme, ThemeProvider} from "@mui/material";
+import CssBaseline from "@mui/material/CssBaseline";
 
 // Page Views
 import TasklistView from "./views/TasklistView";
@@ -25,32 +20,40 @@ import { getCookie } from "./context";
 
 // React Query Configuration
 // Docs: https://tanstack.com/query/latest/docs/react/overview
-const defaultQueryFn = async ({queryKey}) =>{
-
+const defaultQueryFn = async ({ queryKey }) => {
   const formattedUrl = new URL(
-      `${window.location.protocol}${window.location.host}/api/${queryKey[0]}/${!!queryKey.at(1) ? queryKey.at(1) + "/" : ""}`
-    )
+    `${window.location.protocol}${window.location.host}/api/${queryKey[0]}/${
+      !!queryKey.at(1) ? queryKey.at(1) + "/" : ""
+    }`
+  );
 
   const res = await fetch(formattedUrl);
   const data = await res.json();
   return data;
-}
-const defaultMutationFn = async ({model, data}) =>{
-
-  const updateUrl = new URL(`${window.location.protocol}${window.location.host}/api/${model.modelName}/${data.id ? data.id + '/' : ''}`)
+};
+const defaultMutationFn = async ({ model, data }) => {
+  const updateUrl = new URL(
+    `${window.location.protocol}${window.location.host}/api/${
+      model.modelName
+    }/${data.id ? data.id + "/" : ""}`
+  );
   const requestHeaders = new Headers();
-  requestHeaders.set('Content-Type', 'application/json');
-  requestHeaders.set('X-CSRFToken', getCookie('csrftoken'))
+  requestHeaders.set("Content-Type", "application/json");
+  requestHeaders.set("X-CSRFToken", getCookie("csrftoken"));
 
-  return fetch( updateUrl, {method:"PUT", headers:requestHeaders, body:JSON.stringify(data)} )
-}
+  return fetch(updateUrl, {
+    method: "PUT",
+    headers: requestHeaders,
+    body: JSON.stringify(data),
+  });
+};
 const defaultGetNextPageFn = (lastPage, pages) => {
   if (!lastPage.next) {
     return undefined;
   }
   const nextPage = new URL(lastPage.next);
-  return nextPage.searchParams.get('page');
-}
+  return nextPage.searchParams.get("page");
+};
 const defaultHasNextPageFn = (lastPage, pages) => new Boolean(lastPage.next);
 
 const defaultStaleTimeInHours = 1;
@@ -61,13 +64,13 @@ export const queryClient = new QueryClient({
       queryFn: defaultQueryFn,
       getNextPageParam: defaultGetNextPageFn,
       hasNextPage: defaultHasNextPageFn,
-      staleTime: defaultStaleTimeInHours * 60 * 60 * 1000
+      staleTime: defaultStaleTimeInHours * 60 * 60 * 1000,
     },
     mutations: {
       mutationFn: defaultMutationFn,
-    }
-  }
-})
+    },
+  },
+});
 
 // Primary React Component
 const App = () => {
@@ -76,48 +79,52 @@ const App = () => {
 
   return (
     <React.StrictMode>
-          <QueryClientProvider client={queryClient}>
-            <ThemeProvider theme={theme}>
-            <CssBaseline />
-              <Router>
-                <Routes>
-                    <Route
-                      path="/"
-                      element={<CustomPage view={ManageShipmentView} title="Homepage"/>}
-                    >
-                    </Route>
-                    <Route
-                      path="/shipments"
-                      element={<CustomPage view={ManageShipmentView} title="Manage Shipments"/>}
-                    >
-                    </Route>
-                    <Route
-                      path="/shipments/:id"
-                      element={<CustomPage view={ShipmentDetailView} title="Manage Shipment"/>}
-                    >
-                    </Route>
-                    <Route
-                      path="/assets"
-                      element={<CustomPage view={null}/>}>
-                    </Route>
-                    <Route
-                      path="/tasklist"
-                      element={<CustomPage view={TasklistView} title="Tasklist"/>}>
-                    </Route>
-                    <Route
-                      path="/wiki"
-                      element={<CustomPage view={null}/>}>
-                    </Route>
-                    <Route
-                      path="/events/:id"
-                      element={
-                        <CustomPage view={EventDetailView}/>
-                      }>
-                    </Route>
-                  </Routes>
-              </Router>
-            </ThemeProvider>
-          </QueryClientProvider>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Router>
+            <Routes>
+              <Route
+                path="/"
+                element={
+                  <CustomPage view={ManageShipmentView} title="Homepage" />
+                }
+              ></Route>
+              <Route
+                path="/shipments"
+                element={
+                  <CustomPage
+                    view={ManageShipmentView}
+                    title="Manage Shipments"
+                  />
+                }
+              ></Route>
+              <Route
+                path="/shipments/:id"
+                element={
+                  <CustomPage
+                    view={ShipmentDetailView}
+                    title="Manage Shipment"
+                  />
+                }
+              ></Route>
+              <Route
+                path="/assets"
+                element={<CustomPage view={null} />}
+              ></Route>
+              <Route
+                path="/tasklist"
+                element={<CustomPage view={TasklistView} title="Tasklist" />}
+              ></Route>
+              <Route path="/wiki" element={<CustomPage view={null} />}></Route>
+              <Route
+                path="/events/:id"
+                element={<CustomPage view={EventDetailView} />}
+              ></Route>
+            </Routes>
+          </Router>
+        </ThemeProvider>
+      </QueryClientProvider>
     </React.StrictMode>
   );
 };
