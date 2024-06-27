72521b5125735b1f752892143b998636c0e12392
Branden Yarbrough <brandenly@gmail.com>
2024-06-27 16:34:35 -0700

    added useEffect dependancy

--- main/src/forms/CreateShipmentForm.js
+++ main/src/forms/CreateShipmentForm.js
@@ -4,19 +4,21 @@ import { Grid } from '@mui/material';
 
 const CreateShipmentForm = props => {
   
-  const { onChange:externalOnChange, initialValue, key } = props;
+  const { onChange:externalOnChange, initialValue, index } = props;
   const shipmentOptions = useModelOptions('shipment');
 
   const formFields = useModelFormFields({modelOptions:shipmentOptions, excludeReadOnly:true});
 
   useEffect(() => {
-    // console.log(formFields);
-  })
+    externalOnChange(index, formFields)
+  }, [formFields])
+
+  // raise error if no key is provided.
 
   return (
     <Grid container spacing={2} sx={{padding:0, margin:0, maxWidth: "100%"}}>
 
-      <Grid item xs={6}>{formFields.fields["id"]?.inputComponent}</Grid><Grid item xs={6} />
+      <Grid item xs={6}>{formFields.fields["status"]?.inputComponent}</Grid><Grid item xs={6} />
 
       <Grid item xs={6}>{formFields.fields["departure_date"]?.inputComponent}</Grid>
       <Grid item xs={6}>{formFields.fields["arrival_date"]?.inputComponent}</Grid>
@@ -25,8 +27,6 @@ const CreateShipmentForm = props => {
 
       <Grid item xs={6}>{formFields.fields["origin"]?.inputComponent}</Grid>
       <Grid item xs={6}>{formFields.fields["destination"]?.inputComponent}</Grid>
-
-      <Grid item xs={6}>{formFields.fields["status"]?.inputComponent}</Grid>
 
     </Grid>
   )
