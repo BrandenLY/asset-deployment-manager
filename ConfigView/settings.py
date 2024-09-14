2d179429e4623a34301b5ab7536c7777f6db03b1
Branden Yarbrough <brandenly@gmail.com>
2024-09-14 04:40:04 -0700

    update settings

--- ConfigView/settings.py
+++ ConfigView/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 from datetime import timedelta
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fkxmi$4q&icj@vi@c1dgev$m$txbpbhhnu(h(uodx5ds%um*qp'
+SECRET_KEY = "d7T1JA8dBBaFa0yMY+KVNfb3i/8aMLIrYepgxTN8sTk="
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
