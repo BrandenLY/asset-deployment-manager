c188a1de38324c8d48d12a1eb9c79013d9822712
BrandenLY <brandenly@gmail.com>
2024-09-14 20:24:11 -0700

    add logentry serializer

--- api/serializers/base_serializers.py
+++ api/serializers/base_serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from django.contrib.admin.models import LogEntry
 from django.contrib.contenttypes.models import ContentType
 
 class CustomBaseSerializer(serializers.ModelSerializer):
@@ -17,4 +18,18 @@ class ContentTypeSerializer(CustomBaseSerializer):
             "id",
             "app_label",
             "model"
+        ]
+
+class LogEntrySerializer(CustomBaseSerializer):
+    class Meta:
+        model=LogEntry
+        fields=[
+            "id",
+            "object_id",
+            "object_repr",
+            "action_flag",
+            "change_message",
+            "content_type_id",
+            "user_id",
+            "action_time"
         ]
