f6da08445823cb8318c568ef3d45de3d9d3440d8
Branden Yarbrough <brandenly@gmail.com>
2024-11-05 02:39:28 -0800

    Correct state management within model qty selectors

--- main/src/views/EquipmentHolds.js
+++ main/src/views/EquipmentHolds.js
@@ -1,6 +1,6 @@
 import React, { useContext, useState } from "react";
 import { Box, FormControl, InputLabel, OutlinedInput, Paper, TextField, Typography } from "@mui/material";
-import { useInfiniteQuery, useQueryClient } from "@tanstack/react-query";
+import { useInfiniteQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import SortingGrid from "../components/SortingGrid";
 import ModelListControls from "../components/ModelListControls";
 import { backendApiContext, notificationContext } from "../context";
@@ -14,15 +14,47 @@ const CREATEEQUIPMENTHOLDFORMLAYOUT = [];
 const EquipmentHolds = props => {
   
   // Props Destructuring
-  const {} = props;
+  const {addNotif} = props;
+
+  // Hooks
+  const backend = useContext(backendApiContext);
 
   // State
+  const [reserveQty, setReserveQty] = useState({});
   const [newReservationEndDate, setNewReservationEndDate] = useState(null);
   const [newReservationsStartDate, setNewReservationStartDate] = useState(null);
 
   // Queries
   const models = useInfiniteQuery({queryKey:['model']});
   const equipmentholds = useInfiniteQuery({queryKey:[MODELNAME]});
+
+  // Mutations
+  const pushReservations = useMutation({
+    mutationFn : (reservation) => {
+
+      const updateUrl = new URL(`${backend.api.baseUrl}/${MODELNAME}/`);
+      const requestHeaders = backend.api.getRequestHeaders();
+    
+      return fetch(updateUrl, {
+        method: 'POST',
+        headers: requestHeaders,
+        body: JSON.stringify(data),
+      });
+
+    },
+    onSuccess: (data, error, variables, context) => {
+      
+    }
+  })
+
+  // Callback Functions
+  const updateModelQty = (modelId, qty) => {
+    setReserveQty(prev => {
+      let newQty = {...prev};
+      newQty[modelId] = qty;
+      return(newQty);
+    })
+  }
 
   // Formatted Data
   const allLoadedEquipmentHolds = equipmentholds.data?.pages.map(p => p.results).flat();
@@ -89,7 +121,21 @@ const EquipmentHolds = props => {
             <Typography variant="h5">2. Select reservation quantities</Typography>
             <Box display="flex" gap={1} justifyContent="center" alignItems="center" flexDirection="column">
               {allLoadedModels &&
-                allLoadedModels.map(m => <EquipmentSelectionRow key={m.id} model={m} />)
+                allLoadedModels.map(m => {
+
+                  const mQty = reserveQty[m.id]
+                  const value = mQty != undefined ? mQty : 0;
+
+                  return(
+                    <EquipmentSelectionRow
+                      key={m.id} 
+                      model={m} 
+                      onChange={qty => { updateModelQty(m.id, qty) }} 
+                      value={value}
+                    />
+                  )
+
+                })
               }
             </Box>
           </Box>
@@ -98,6 +144,7 @@ const EquipmentHolds = props => {
       </Section>
       <Section
         title="Existing Reservations"
+        actions={[<ModelListControls model={MODELNAME} createObjectsFormLayout={CREATEEQUIPMENTHOLDFORMLAYOUT}/>]}
       >
         <SortingGrid 
           title="Equipment Reservations"
@@ -115,12 +162,14 @@ const EquipmentHolds = props => {
 const EquipmentSelectionRow = props => {
 
   // Props Destructuring
-  const {model} = props;
+  const {model, value, onChange} = props;
 
   return(
     <Box display="flex" maxWidth="450px" width="100%">
       <Box flexGrow={1} display="flex" alignItems="center">{model.label}</Box>
-      <Box><IntegerSelector /></Box>
+      <Box>
+        <IntegerSelector onChange={onChange} value={value}/>
+      </Box>
     </Box>
   );
 
