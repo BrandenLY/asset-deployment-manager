22a7de31cab16db8fd522d768de174e09c29e15a
Branden Yarbrough <brandenly@gmail.com>
2025-03-19 21:45:52 -0700

    Remove references to deleted django app serializers.

--- api/serializers/__init__.py
+++ api/serializers/__init__.py
@@ -14,7 +14,3 @@ from .user_serializer import UserSerializer
 from .user_serializer import GroupSerializer
 from .user_serializer import PermissionSerializer
 from .event_serializer import EventSerializer
-# App Related Imports : Tasklist
-from .event_serializer import ProjectSerializer
-from .service_serializer import ServiceSerializer
-from .remark_serializers import MilestoneSerializer
--- api/serializers/event_serializer.py
+++ api/serializers/event_serializer.py
@@ -1,30 +1,9 @@
 from rest_framework import serializers
 from main.models import Event
-from tasklist.models import Project
 from .base_serializers import CustomBaseSerializer
 
 
-class ProjectSerializer(CustomBaseSerializer):
-
-    class Meta:
-        model = Project
-        fields = [
-            "label",
-            "printer_type",
-            "production_redwood_id",
-            "production_show_code",
-            "test_redwood_id",
-            "test_show_code",
-            "account_manager",
-            "lead_retrieval_specialist",
-            "project_manager",
-            "solutions_specialist",
-            "milestones",
-            "services"
-        ]
-
 class EventSerializer(CustomBaseSerializer):
-    project = ProjectSerializer(read_only=True)
     class Meta:
         model = Event
         fields = [
